391e8d33678efba872486e90074aed3ccc95ab6b
j3f3rs0n1 <4529@holbertonschool.com>
2022-02-10 21:08:24 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+# include <stdio.h>
+
+/**
+ * main - print size
+ * Return: return 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
+
+	return (0);
+}
+

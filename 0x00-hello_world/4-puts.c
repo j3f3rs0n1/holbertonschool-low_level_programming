a5aacc9449816b5df06f5858f4719db4ff3490a5
j3f3rs0n1 <4529@holbertonschool.com>
2022-02-10 18:27:16 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - print "Programming is like building a multilingual puzzle
+ * Return: return 0
+ */
+
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}

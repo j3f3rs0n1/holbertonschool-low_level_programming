4e0e47467f01f0a36a8ce9b3bfdfb4a918f517cd
j3f3rs0n1 <4529@holbertonschool.com>
2022-02-10 18:52:34 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+# include <stdio.h>
+
+/**
+ * main - printf -"with proper grammar, but the outcome is a piece of art,
+ * Return: - return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

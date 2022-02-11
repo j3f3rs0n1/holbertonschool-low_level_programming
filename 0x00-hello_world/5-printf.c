039ce2f4453ee08d07f08e91bc5583af35ef7adc
j3f3rs0n1 <4529@holbertonschool.com>
2022-02-10 20:15:32 -0800

    recuperado

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,25 @@
-# include <stdio.h>
+nclude <stdio.h>
+
+
 
 /**
- * main - printf -"with proper grammar, but the outcome is a piece of art,
- * Return: - return 0
- */
+ *
+ *  * main - printf -"with proper grammar, but the outcome is a piece of art,
+ *
+ *   * Return: - return 0
+ *
+ *    */
+
+
 
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+
+
+			return (0);
+
 }
